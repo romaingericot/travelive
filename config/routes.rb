d675b914ea0cbe466ba936d649e619c5dd13e361
Romain Gericot <romain.gericot@gmail.com>
2020-06-22 17:32:55 +0200

    Add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'bookings/index'
-  get 'bookings/show'
-  get 'tours/index'
-  get 'tours/show'
   devise_for :users
   root to: 'pages#home'
   resources :tours, shallow: true do
