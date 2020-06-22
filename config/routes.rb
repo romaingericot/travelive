3b975657a30ab8f14129bf37683c99378a32eb4a
Romain Gericot <romain.gericot@gmail.com>
2020-06-22 17:31:09 +0200

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'bookings/index'
+  get 'bookings/show'
+  get 'tours/index'
+  get 'tours/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
