a9523cbc5db928f97f6d4a0542d6b51376cf035c
Jerome Toulouse <toulouse.jerome@gmail.com>
2020-06-23 12:40:07 +0200

    validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :tour
   belongs_to :user
+  has_many :reviews
+
+  validates :progress, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :tours
+  has_many :bookings
+
+  validates :email, confirmation: true
+  validates :email_confirmation, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { in: 6..20 }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :description, length: { maximum: 300,
+    too_long: "%{count} characters is the maximum allowed" }
+
 end
