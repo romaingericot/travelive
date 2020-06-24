a6cd3518250bb27f49b730e75e65f1fdc7d2286c
Fabiola Guillou Camargo <facamargo@gmail.com>
2020-06-24 13:36:26 +0200

    Map custom

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -7,7 +7,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/fguilloucamargo/ckbt4oswm0jqq1jnrnacxwpan'
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);
