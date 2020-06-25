8950f47822b96d478a3bbe250d91204c2bfb8c4e
Fabiola Guillou Camargo <facamargo@gmail.com>
2020-06-25 15:10:36 +0200

    add search bar with dropdown

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -2,13 +2,25 @@ class ToursController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :index ]
 
   def index
-    @tours = Tour.geocoded
-    @markers = @tours.map do |tour|
-      {
-        lat: tour.latitude,
-        lng: tour.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { tour: tour })
-      }
+    @cities = Tour.all.map { |tour| tour.city }.sort
+    if params[:search].nil?
+      @tours = Tour.geocoded
+      @markers = @tours.map do |tour|
+        {
+          lat: tour.latitude,
+          lng: tour.longitude,
+          infoWindow: render_to_string(partial: "info_window", locals: { tour: tour })
+        }
+      end
+    else
+      @tours = Tour.where("city ILIKE ?", params[:search][:city]).geocoded
+      @markers = @tours.map do |tour|
+        {
+          lat: tour.latitude,
+          lng: tour.longitude,
+          infoWindow: render_to_string(partial: "info_window", locals: { tour: tour })
+        }
+      end
     end
   end
 
