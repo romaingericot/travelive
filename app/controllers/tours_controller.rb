9728ea91636c228d13636d45b72ea30978ded3a3
Fabiola Guillou Camargo <facamargo@gmail.com>
2020-06-25 17:23:56 +0200

    search with country and city + zoom

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -3,6 +3,7 @@ class ToursController < ApplicationController
 
   def index
     @cities = Tour.all.map { |tour| tour.city }.sort
+    @countries = Tour.all.map { |tour| tour.country }.sort
     if params[:search].nil?
       @tours = Tour.geocoded
       @markers = @tours.map do |tour|
@@ -13,13 +14,12 @@ class ToursController < ApplicationController
         }
       end
     else
-      @tours = Tour.where("city ILIKE ?", params[:search][:city]).geocoded
-      @markers = @tours.map do |tour|
-        {
-          lat: tour.latitude,
-          lng: tour.longitude,
-          infoWindow: render_to_string(partial: "info_window", locals: { tour: tour })
-        }
+      if params[:search][:city].empty?
+        @tours = Tour.where("country ILIKE ?", params[:search][:country]).geocoded
+        set_markers(@tours)
+      else
+        @tours = Tour.where("city ILIKE ?", params[:search][:city]).geocoded
+        set_markers(@tours)
       end
     end
   end
@@ -49,4 +49,16 @@ class ToursController < ApplicationController
   def live
   end
 
+  private
+
+  def set_markers(tours)
+    @markers = tours.map do |tour|
+      {
+        lat: tour.latitude,
+        lng: tour.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { tour: tour })
+      }
+    end
+    @markers
+  end
 end
