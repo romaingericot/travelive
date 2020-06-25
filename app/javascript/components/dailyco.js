ae87f37745e3beb0fe10c0de527bea8278261b22
Romain Gericot <romain.gericot@gmail.com>
2020-06-25 14:41:49 +0200

    master

---
+++ app/javascript/components/dailyco.js
@@ -0,0 +1,8 @@
+// Create a daily.co client
+const daily = new Daily("3dd17e0fcbb5bddf8650753d5d46c6a5cfbd56d08283eac3402804b58970e8c4");
+
+// Create a room
+const room = await daily.createRoom({ privacy: "public" });
+
+// Get info about a room
+console.log(await daily.room(room.name));
