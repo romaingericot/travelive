f0b47458afa051ef96ed741af5e4f06162c8b168
Romain Gericot <romain.gericot@gmail.com>
2020-06-25 16:43:09 +0200

    DailyColive

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -22,10 +22,12 @@ class ToursController < ApplicationController
 
   def new
     @tour = Tour.new
+    @tours = Review.where(user_id: current_user)
   end
 
   def create
-
+    @tour = Tour.new(tour_params)
+    @tour.user = current_user
 
     url = URI("https://api.daily.co/v1/rooms")
 
@@ -44,23 +46,31 @@ class ToursController < ApplicationController
     @tour.live_url = JSON.parse(response.read_body)["url"]
 
     if @tour.save
+      redirect_to tours_path
+    else
+      render :new
     end
-
   end
 
   def edit
   end
 
   def update
-
   end
 
   def destroy
+    @tour = Tour.find(params[:id])
+    @tour.destroy
+    redirect_to tours_path
   end
 
   def live
+    @tour = Tour.find(params[:tour_id])
+  end
 
+  private
 
-
+  def tour_params
+    params.require(:tour).permit(:name, :city, :country, :capacity, :date, :time, :duration, :description, :language, :category, :price)
   end
 end
