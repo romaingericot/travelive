4939fa0b323d476399793e8995c7fad619e32fff
Fabiola Guillou Camargo <facamargo@gmail.com>
2020-06-25 12:14:28 +0200

    savind

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,8 +1,28 @@
 class ToursController < ApplicationController
+  # skip_before_action :authenticate_user!, only: [ :index ]
+
   def index
   end
 
   def show
     @tour = Tour.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  resources :tours, shallow: true do
-    resources :bookings do
-    resources :reviews
+  root to: 'tours#index'
+  resources :tours shallow: true do
+    resources :bookings, only: [:new, :create, :show, :destroy] do
+    resources :reviews, only: [:new, :create, :destroy]
     end
   end
+  resources :users, only: [:show]
 end
