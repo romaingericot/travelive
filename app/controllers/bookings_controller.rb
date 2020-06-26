1f355d96872433c48ab8e8b09e6ece41a8bc0b49
Fabiola Guillou Camargo <facamargo@gmail.com>
2020-06-26 15:16:32 +0200

    create and destroy booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,10 +3,16 @@ class BookingsController < ApplicationController
     @booking = Booking.all
   end
 
-  def new
-    @booking = Booking.new
+  def create
+    @booking = Booking.new(user: current_user, tour: Tour.find(params[:tour_id]), progress: 0)
+    if @booking.save
+      redirect_to user_path(current_user)
+    end
   end
 
-  def create
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to user_path(current_user)
   end
 end
