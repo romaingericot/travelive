9d8e10557450fdddecd97edf523ba8fd702f87b9
Romain Gericot <romain.gericot@gmail.com>
2020-06-30 14:25:25 +0200

    live, counter, hotfix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ require("@rails/activestorage").start()
 require("channels")
 // require("flickity-fade")
 
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -34,6 +36,8 @@ import 'aos/dist/aos.css';
 // Internal imports
 import { initMapbox } from '../plugins/init_mapbox';
 import { initUpdateNavbarOnScroll } from '../components/navbar';
+import { liveTimer } from '../plugins/init_count_up'
+import { initCountDown } from '../components/countdownclock'
 // import { CountUp } from '../plugins/countUp.js';
 // import { initSelect2 } from '../components/init_select2';
 // import { Flickity } from '../plugins/init_flickity-fade';
@@ -44,5 +48,8 @@ document.addEventListener('turbolinks:load', () => {
   initMapbox();
   AOS.init();
   jarallaxVideo();
+  liveTimer();
+  initCountDown();
+  // countUp.start();
   // Flickity();
 });
