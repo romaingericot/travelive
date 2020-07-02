04656ec0130faee9b97e489fc6abce847519a01e
Romain Gericot <romain.gericot@gmail.com>
2020-07-02 09:11:55 +0200

    fixing map on live

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,11 +5,10 @@ const initMapbox = () => {
 
   if (mapElement) {
     const fitMapToMarkers = (map, markers) => {
-    const bounds = new mapboxgl.LngLatBounds();
-    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 5, duration: 0 });
+      const bounds = new mapboxgl.LngLatBounds();
+      markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+      map.fitBounds(bounds, { padding: 70, maxZoom: 5, duration: 0 });
     };
-
     if (mapElement) { // only build a map if there's a div#map to inject into
       mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
       const map = new mapboxgl.Map({
@@ -18,7 +17,6 @@ const initMapbox = () => {
         center: [0, 15],
         zoom: 0.99
       });
-
       const markers = JSON.parse(mapElement.dataset.markers);
       markers.forEach((marker) => {
         const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
