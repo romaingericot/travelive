0c9af29784ee4cbdd8883609f56d286ec68d1b5f
Edouard Couten <edouard.couten@gmail.com>
2020-09-17 09:59:22 +0200

    change

--- db/migrate/20200630160652_add_progress_to_checkpoints.rb
+++ db/migrate/20200630160652_add_progress_to_checkpoints.rb
@@ -1,4 +1,4 @@
-class AddProgressToCheckpoints < ActiveRecord::Migration[6.0]
+xclass AddProgressToCheckpoints < ActiveRecord::Migration[6.0]
   def change
     add_column :checkpoints, :progress, :integer
   end
