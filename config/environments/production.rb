3af8431a0219686dbf59d50d0b4ff43ed9348010
Romain Géricot <61696786+romaingericot@users.noreply.github.com>
2020-09-18 11:04:34 +0200

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://parisfrenchburger.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: "https://www.travelive.fr" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
